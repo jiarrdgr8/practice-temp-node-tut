152e9384cb2ff8fd40abba703a8aa1d22ab06958
unknown <jiarrdgr8@gmail.com>
2023-01-07 00:12:50 +0800

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,4 @@
+console.log('s');
+setInterval(()=>{
+  console.log('hello world');
+}, 2000)
---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+
+const john = 'john'
+const susan = 'susan'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('Jiarr')
+sayHi(john)
+sayHi(susan)
